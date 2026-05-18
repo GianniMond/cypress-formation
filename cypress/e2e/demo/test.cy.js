ddd6647024105ed6d417629c22fdd593a2042f55
Gianni Mondello <giannimondello@Host-005.lan>
2026-05-18 16:59:04 +0200

    refacto demo

--- cypress/e2e/demo/test.cy.js
+++ cypress/e2e/demo/test.cy.js
@@ -1,5 +1,9 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Demo courte hors exos', () => {
+  it('verifie une page de la Kitchen Sink', () => {
+    cy.visit('https://example.cypress.io/commands/network-requests')
+    cy.contains('h1', 'Network Requests').should('be.visible')
+    cy.contains('Get Comment').should('be.visible')
+    cy.url().should('include', '/commands/network-requests')
+    cy.get('.network-btn').first().should('be.visible')
   })
 })
